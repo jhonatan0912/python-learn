b67bf3ffc1d55b7631aacc1d13fe94085659ff3c
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-19 12:26:03 -0500

    second exercise

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+passwordString = "password"
+password = input("write a password: ")
+
+if (passwordString == password):
+    print("Introduced password its equal")
+else:
+    print("please try with other password")
